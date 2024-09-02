3e3466f44f93acc17ad875b45565f5bdd2828e7c
unknown <kumarvijay54569@gmail.com>
2024-09-02 14:03:42 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "C:/Users/Dell/Downloads/PortfolioB2Y/src/**/*.{js,jsx,ts,tsx}",
+    // Add other paths if needed
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#04668D',
+        customBlu: '#DA67AC',
+      },
+    },
+  },
+  plugins: [
+  ],
+}
+
+
+
+
